b76b63d2389d5dab8bfbd7e5664ac876ff4483cd
Saurabh Totey <SaurabhTotey@proton.me>
2025-12-07 18:36:36 -0500

    Refactor canvas code into class

--- script.js
+++ script.js
@@ -9,34 +9,55 @@ window.onresize = () => {
 	canvas.setAttribute("height", `${window.innerHeight}`);
 };
 
-const logicalWidth = 600;
-const logicalHeight = 400;
+class Canvas {
 
-const getCanvasPhysicalSize = () => [parseInt(canvas.getAttribute("width")), parseInt(canvas.getAttribute("height"))];
-const getCanvasDrawingAreaSize = () => {
-    const logicalAspectRatio = logicalWidth / logicalHeight;
-    const [physicalWidth, physicalHeight] = getCanvasPhysicalSize();
-    const physicalAspectRatio = physicalWidth / physicalHeight;
-    let drawingAreaWidth = physicalWidth;
-    let drawingAreaHeight = physicalHeight;
-    if (logicalAspectRatio < physicalAspectRatio) { // physical canvas is wider than logical drawing area, limited by height
-        drawingAreaWidth = logicalAspectRatio * physicalHeight;
-    } else { // physical canvas is taller than logical drawing area, limited by width
-        drawingAreaHeight = physicalWidth / logicalAspectRatio;
+    constructor(canvasElement, renderer, logicalWidth, logicalHeight) {
+        this.canvas = canvasElement;
+        this.renderer = renderer;
+        this.logicalWidth = logicalWidth;
+        this.logicalHeight = logicalHeight;
     }
-    return [drawingAreaWidth, drawingAreaHeight];
-};
 
-const convertLogicalCoordinatesToPhysical = (logicalX, logicalY) => {
-    const [physicalWidth, physicalHeight] = getCanvasPhysicalSize();
-    const [drawingAreaWidth, drawingAreaHeight] = getCanvasDrawingAreaSize();
-    let physicalX = (logicalX / logicalWidth) * drawingAreaWidth + (physicalWidth - drawingAreaWidth) / 2;
-    let physicalY = (logicalY / logicalHeight) * drawingAreaHeight + (physicalHeight - drawingAreaHeight) / 2;
-    return [physicalX, physicalHeight - physicalY];
-};
+    getPhysicalSize() {
+        return [parseInt(this.canvas.getAttribute("width")), parseInt(this.canvas.getAttribute("height"))];
+    }
+
+    getDrawingAreaSize() {
+        const logicalAspectRatio = this.logicalWidth / this.logicalHeight;
+        const [physicalWidth, physicalHeight] = this.getPhysicalSize();
+        const physicalAspectRatio = physicalWidth / physicalHeight;
+        let drawingAreaWidth = physicalWidth;
+        let drawingAreaHeight = physicalHeight;
+        if (logicalAspectRatio < physicalAspectRatio) { // physical canvas is wider than logical drawing area, limited by height
+            drawingAreaWidth = logicalAspectRatio * physicalHeight;
+        } else { // physical canvas is taller than logical drawing area, limited by width
+            drawingAreaHeight = physicalWidth / logicalAspectRatio;
+        }
+        return [drawingAreaWidth, drawingAreaHeight];
+    }
+
+    convertLogicalCoordinatesToPhysical(logicalX, logicalY) {
+        const [physicalWidth, physicalHeight] = this.getPhysicalSize();
+        const [drawingAreaWidth, drawingAreaHeight] = this.getDrawingAreaSize();
+        let physicalX = (logicalX / this.logicalWidth) * drawingAreaWidth + (physicalWidth - drawingAreaWidth) / 2;
+        let physicalY = (logicalY / this.logicalHeight) * drawingAreaHeight + (physicalHeight - drawingAreaHeight) / 2;
+        return [physicalX, physicalHeight - physicalY];
+    }
+
+    clear() {
+        this.renderer.clearRect(0, 0, ...this.getPhysicalSize());
+    }
+
+    drawDrawingArea() {
+        this.renderer.strokeRect(...this.convertLogicalCoordinatesToPhysical(0, this.logicalHeight), ...this.getDrawingAreaSize());
+    }
+
+}
+
+const drawingCanvas = new Canvas(canvas, renderer, 600, 400);
 
 window.setInterval(() => {
-    renderer.clearRect(0, 0, ...getCanvasPhysicalSize());
-    renderer.strokeRect(...convertLogicalCoordinatesToPhysical(0, logicalHeight), ...getCanvasDrawingAreaSize());
+    drawingCanvas.clear();
+    drawingCanvas.drawDrawingArea();
     // TODO:
 }, 17);
